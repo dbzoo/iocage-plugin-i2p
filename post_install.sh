4300f4b55dd8e3bd7c5d54701e7802d2379e54fd
dbzoo <brett@dbzoo.com>
2019-06-23 13:24:23 -0400

    Initial commit

---
+++ post_install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pw add user -n i2p -c I2P -s /bin/sh -m
+
+sysrc -f /etc/rc.conf i2p_enable=YES
+sysrc -f /etc/rc.conf i2p_user=i2p
+
+/usr/local/etc/rc.d/i2p install
+
+sed -i -e '/^wrapper.java.command=/s#=java#=/usr/local/openjdk8/bin/java#' /home/i2p/i2p/wrapper.config
+sed -i -e 's#$INSTALL_PATH#/home/i2p/i2p#' /home/i2p/i2p/wrapper.config
+sed -i -e '/^clientApp.0.args=7657 ::1/s/^/#/' /home/i2p/i2p/clients.config
+sed -i -e '/^#clientApp.0.args=7657 0.0.0.0/s/#//' /home/i2p/i2p/clients.config
+
+/usr/local/etc/rc.d/i2p start
